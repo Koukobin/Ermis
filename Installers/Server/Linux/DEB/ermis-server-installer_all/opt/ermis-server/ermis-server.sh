31c741ffb525e599f15e5473db25c1387418649a
Ilias <ilias.koukovinis@gmail.com>
2025-06-22 00:17:15 +0300

    Revert apply_server_settings.pl changes on server shutdown to avoid bugs

--- Installers/Server/Linux/DEB/ermis-server-installer_all/opt/ermis-server/ermis-server.sh
+++ Installers/Server/Linux/DEB/ermis-server-installer_all/opt/ermis-server/ermis-server.sh
@@ -15,32 +15,81 @@
 # You should have received a copy of the GNU Affero General Public License
 # along with this program. If not, see <https://www.gnu.org/licenses/>.
 
-sudo ./apply_server_settings.pl
+debute_server () {
+    sudo ./apply_server_settings.pl
 
-# Attempt to find server JAR file
-ERMIS_SERVER_JAR_FILE=$(find bin -maxdepth 1 -name "*.jar" -print -quit)
+    # Attempt to find server JAR file
+    ERMIS_SERVER_JAR_FILE=$(find bin -maxdepth 1 -name "*.jar" -print -quit)
 
-# Check if a JAR file was found
-if [ -z "$ERMIS_SERVER_JAR_FILE" ]; then
-  echo "No JAR file found in the directory!"
-  exit 1
-fi
+    # Check if a JAR file was found
+    if [ -z "$ERMIS_SERVER_JAR_FILE" ]; then
+      echo "No JAR file found in the directory!"
+      exit 1
+    fi
 
-VM_ARGUMENTS="
--Djava.security.egd=file:/dev/./urandom 
--server 
--XX:+UseZGC 
---add-opens java.base/java.lang=ALL-UNNAMED 
---add-opens java.base/jdk.internal.misc=ALL-UNNAMED
---add-opens java.base/java.nio=ALL-UNNAMED
--Dio.netty.tryReflectionSetAccessible=true
--Dfile.encoding=UTF-8"
-PROGRAM_ARGUMENTS=""
+    VM_ARGUMENTS="
+    -Djava.security.egd=file:/dev/./urandom 
+    -server 
+    -XX:+UseZGC 
+    --add-opens java.base/java.lang=ALL-UNNAMED 
+    --add-opens java.base/jdk.internal.misc=ALL-UNNAMED
+    --add-opens java.base/java.nio=ALL-UNNAMED
+    -Dio.netty.tryReflectionSetAccessible=true
+    -Dfile.encoding=UTF-8"
+    PROGRAM_ARGUMENTS=""
 
-if [ -n "$JAVA_HOME" ]; then
-  sudo $JAVA_HOME/bin/java -jar $VM_ARGUMENTS $ERMIS_SERVER_JAR_FILE $PROGRAM_ARGUMENTS
-else
-  sudo java -jar $VM_ARGUMENTS $ERMIS_SERVER_JAR_FILE $PROGRAM_ARGUMENTS
-fi
+    if [ -n "$JAVA_HOME" ]; then
+      sudo $JAVA_HOME/bin/java -jar $VM_ARGUMENTS $ERMIS_SERVER_JAR_FILE $PROGRAM_ARGUMENTS
+    else
+      sudo java -jar $VM_ARGUMENTS $ERMIS_SERVER_JAR_FILE $PROGRAM_ARGUMENTS
+    fi
 
-sudo turnserver --log-file stdout --tls-listening-port=5439 --listening-port=5440
+    sudo turnserver --log-file stdout --tls-listening-port=5439 --listening-port=5440
+}
+
+create_folder_backup_and_restore_once_process_dies () {
+    touch /tmp/session.keepalive # File denoting process is active
+
+    kill_file_denoting_process_is_alive() {
+        rm -f /tmp/session.keepalive # Kill file denoting process is active
+    }
+
+    trap kill_file_denoting_process_is_alive EXIT
+    # UPPERCASE IS CRUCIAL SINCE THE VARIABLES NOW ARE GLOBAL AND THE TRAP WORKS
+    FOLDER="$1/"
+    BACKUP="$1_backup/"
+
+    restore_backup() {
+        echo "\nRestoring original folder: $BACKUP -> $FOLDER ..."
+        sudo rm -rf "$FOLDER"
+        sudo cp -r "$BACKUP" "$FOLDER"
+        sudo chmod -R 777 "$FOLDER"
+        sudo rm -rf "$BACKUP"
+    }
+    
+    # If backup exists then perform cleanup
+    if [ -d "$BACKUP" ]; then
+        restore_backup
+    fi
+
+    (
+        while [ -f /tmp/session.keepalive ]; do
+            sleep 1
+        done
+
+        echo "Parent shell died. Exiting subshell."
+        trap restore_backup EXIT INT TERM
+    ) &
+
+    # Copy the original directory to a backup location
+    sudo cp -r --no-preserve=ownership "$FOLDER" "$BACKUP"
+
+    echo "Created backup for server folder $FOLDER -> $BACKUP..."
+
+    # Folder automatically restored by subshell above
+}
+
+create_folder_backup_and_restore_once_process_dies "/var/ermis-server/www"
+create_folder_backup_and_restore_once_process_dies "/etc/nginx"
+
+debute_server
