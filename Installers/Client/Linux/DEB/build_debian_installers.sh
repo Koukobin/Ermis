110a56aec4a0e61351f15c472954141e9240b542
Koukobin <ilias.koukovinis@gmail.com>
2025-06-30 00:00:14 -1100

    Added arm64 installer and multi-architecture (all) installer support for the desktop client

---
+++ Installers/Client/Linux/DEB/build_debian_installers.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+# Define variables for folder and file paths
+TARGET_FOLDER="$(git rev-parse --show-toplevel)/ErmisClient/Desktop"
+JAR_FILE="$TARGET_FOLDER/target/ermis-client.jar"
+LIB_FOLDER="$TARGET_FOLDER/target/lib"
+
+SOURCE_FOLDER="$(git rev-parse --show-toplevel)/ErmisClient/Desktop"
+TARGET_JAR="$SOURCE_FOLDER/target/ermis-client.jar"
+LIB_FOLDER="$SOURCE_FOLDER/target/lib"
+DOC_FILES=("README.md" "LICENSE.txt" "NOTICE.txt")
+
+update_files () {
+    INSTALL_FOLDER="$1"
+    BIN="$INSTALL_FOLDER/bin"
+
+    # Create necessary directories
+    mkdir -p "$BIN/lib"
+
+    # Copy the main JAR file and libraries
+    cp "$JAR_FILE" "$BIN" || { echo "Failed to copy ermis-client.jar ($JAR_FILE)"; exit 1; }
+    cp "$LIB_FOLDER"/* "$BIN/lib" || { echo "Failed to copy library files ($LIB_FOLDER)"; exit 1; }
+
+    # Copy documentation files
+    for file in "${DOC_FILES[@]}"; do
+        cp "$TARGET_FOLDER/$file" "$INSTALL_FOLDER" || { echo "Failed to copy $file"; exit 1; }
+    done
+}
+
+create_deb_package() {
+    sudo dpkg-deb --build $1 || { echo "Failed to build DEB package: $1"; exit 1; }
+    echo "DEB package - $1 - succesfully created!"
+}
+
+update_files "ermis-client-installer_amd64/opt/Ermis-Client"
+update_files "ermis-client-installer_arm64/opt/Ermis-Client"
+update_files "ermis-client-installer_all/opt/Ermis-Client"
+
+# Create DEB packages
+create_deb_package "ermis-client-installer_amd64"
+create_deb_package "ermis-client-installer_arm64"
+create_deb_package "ermis-client-installer_all"
+
+echo "Succesfully created all DEB packages!"
