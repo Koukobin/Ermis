11888d092e98baccbc01583fcfc955dbd64605e6
Koukobin <ilias.koukovinis@gmail.com>
2025-07-02 18:15:15 +0300

    Added build file to root directory of the source tree which builds entire project

---
+++ build.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Build ErmisCommon, ErmisServer and ErmisClient (Desktop)
+mvn clean install
+
+# Navigate to the mobile client directory
+cd ErmisClient/Mobile/ermis_client/ || {
+  echo "Failed to change directory to Ermis-Client/Mobile/ermis_client/"
+  exit 1
+}
+
+chmod +x build.sh
+./build.sh
+
