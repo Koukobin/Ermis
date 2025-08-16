f1c27cd53a030722b971937ed392348e6dd1d01a
Koukobin <ilias.koukovinis@gmail.com>
2025-08-16 20:07:50 +0300

    fix: remove redundant build step

--- ErmisClient/Mobile/ermis_client/build.sh
+++ ErmisClient/Mobile/ermis_client/build.sh
@@ -5,9 +5,3 @@ flutter pub get
 dart run flutter_launcher_icons
 dart run intl_utils:generate
 dart run build_runner build
-
-# Running "dart run flutter_launcher_icons" generates a folder named 
-# "mipmap-anydpi-v26" in android/app/src/main/res/. In order to ensure the app icon 
-# is correctly displayed in the application launcher, the aforementioned folder must be deleted.
-# I have no idea why, but it works.
-rm -r android/app/src/main/res/mipmap-anydpi-v26
