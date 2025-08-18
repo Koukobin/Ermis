13e7f391eeb285a6ceba9f75d87fac3bf09d1be9
Koukobin <ilias.koukovinis@gmail.com>
2025-08-18 17:23:41 -0500

    Remove redundant space

--- ErmisClient/Mobile/ermis_client/lib/core/services/navigation_service.dart
+++ ErmisClient/Mobile/ermis_client/lib/core/services/navigation_service.dart
@@ -16,7 +16,7 @@
 
 import 'package:flutter/widgets.dart';
 
-/// Service which keeps global [BuildContext] of App, so 
+/// Service which keeps global [BuildContext] of App, so
 /// I don't to have to constantly pass around the context
 class NavigationService {
   static GlobalKey<NavigatorState> navigatorKey = GlobalKey<NavigatorState>();
