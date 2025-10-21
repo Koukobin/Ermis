8626eb090e6074249998427c9ad33dbfdff0671a
Koukobin <ilias.koukovinis@gmail.com>
2025-10-21 22:19:08 +0300

    commit of the day

--- ErmisServer/Installers/Unix/DEB/build_debian_installer.sh
+++ ErmisServer/Installers/Unix/DEB/build_debian_installer.sh
@@ -14,6 +14,7 @@
 # 
 # You should have received a copy of the GNU Affero General Public License
 # along with this program. If not, see <https://www.gnu.org/licenses/>.
+#
 
 # Define variables for folder and file paths
 TARGET_FOLDER="$(git rev-parse --show-toplevel)/ErmisServer"
