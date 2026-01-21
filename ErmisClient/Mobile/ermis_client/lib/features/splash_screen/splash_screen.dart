507ec2a816cf35de67a6c525ad765f19c1b105a2
Koukobin <ilias.koukovinis@gmail.com>
2026-01-21 16:01:15 +0300

    feat(mobile): introduce animated text widgets to enchance UI engagement

--- ErmisClient/Mobile/ermis_client/lib/features/splash_screen/splash_screen.dart
+++ ErmisClient/Mobile/ermis_client/lib/features/splash_screen/splash_screen.dart
@@ -14,6 +14,8 @@
  * along with this program. If not, see <https://www.gnu.org/licenses/>.
  */
 
+import 'dart:math';
+
 import 'package:ermis_mobile/core/services/database/extensions/servers_extension.dart';
 import 'package:ermis_mobile/core/services/database/models/server_info.dart';
 import 'package:ermis_mobile/core/util/dialogs_utils.dart';
@@ -24,6 +26,9 @@ import '../../core/util/ermis_loading_messages.dart';
 import '../choose_server_screen/choose_server_screen.dart';
 import '../../constants/app_constants.dart';
 import '../../core/services/database/database_service.dart';
+import '../../core/widgets/animated_text/animated_text.dart';
+import '../../core/widgets/animated_text/scramble.dart';
+import '../../core/widgets/animated_text/typewriter.dart';
 
 class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key});
@@ -66,6 +71,65 @@ class _SplashScreenState extends State<SplashScreen> {
     });
   }
 
+  /// Builds an Ermis-tailored loading text widget with
+  /// slight variations because I could not decide which
+  /// one I preferred best.
+  Widget buildErmisLoadingText() {
+    final random = Random();
+
+    if (random.nextBool()) {
+      return Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 8.0),
+        child: AnimatedTextKit(
+          totalRepeatCount: 1,
+          animatedTexts: [
+            TypewriterAnimatedText(
+              ErmisLoadingMessages.randomMessage(),
+              textAlign: TextAlign.center,
+              textStyle: const TextStyle(
+                fontSize: 18.0,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ],
+        ),
+      );
+    }
+
+    if (random.nextBool()) {
+      return Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 8.0),
+        child: AnimatedTextKit(
+          totalRepeatCount: 1,
+          animatedTexts: [
+            ScrambleAnimatedText(
+              ErmisLoadingMessages.randomMessage(),
+              textAlign: TextAlign.center,
+              textStyle: const TextStyle(
+                fontSize: 18.0,
+                fontWeight: FontWeight.bold,
+              ),
+              speed: const Duration(milliseconds: 25),
+            ),
+          ],
+        ),
+      );
+    }
+
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 8.0),
+      child: Text(
+        ErmisLoadingMessages.randomMessage(),
+        textAlign: TextAlign.center,
+        style: const TextStyle(
+          fontSize: 18,
+          fontStyle: FontStyle.italic,
+          fontWeight: FontWeight.bold,
+        ),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     final appColors = Theme.of(context).extension<AppColors>()!;
@@ -87,19 +151,7 @@ class _SplashScreenState extends State<SplashScreen> {
                   width: 120,
                 ),
               ),
-
-              Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 8.0),
-                child: Text(
-                  ErmisLoadingMessages.randomMessage(),
-                  textAlign: TextAlign.center,
-                  style: const TextStyle(
-                    fontSize: 18,
-                    fontStyle: FontStyle.italic,
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
-              ),
+              buildErmisLoadingText(),
             ],
           ),
         ),
