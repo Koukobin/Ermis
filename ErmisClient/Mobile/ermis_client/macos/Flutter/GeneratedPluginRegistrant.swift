243ac2e130b6d1e21b0e0a25b6e755baa5e88bd6
Koukobin <ilias.koukovinis@gmail.com>
2026-04-06 21:05:21 +0300

    chore(mobile): incude video_player_avfoundation in GeneratedPluginRegistrant

--- ErmisClient/Mobile/ermis_client/macos/Flutter/GeneratedPluginRegistrant.swift
+++ ErmisClient/Mobile/ermis_client/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -16,6 +16,7 @@ import path_provider_foundation
 import shared_preferences_foundation
 import sqflite_darwin
 import url_launcher_macos
+import video_player_avfoundation
 import wakelock_plus
 import zstandard_macos
 
@@ -31,6 +32,7 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
   SqflitePlugin.register(with: registry.registrar(forPlugin: "SqflitePlugin"))
   UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
+  FVPVideoPlayerPlugin.register(with: registry.registrar(forPlugin: "FVPVideoPlayerPlugin"))
   WakelockPlusMacosPlugin.register(with: registry.registrar(forPlugin: "WakelockPlusMacosPlugin"))
   ZstandardMacosPlugin.register(with: registry.registrar(forPlugin: "ZstandardMacosPlugin"))
 }
