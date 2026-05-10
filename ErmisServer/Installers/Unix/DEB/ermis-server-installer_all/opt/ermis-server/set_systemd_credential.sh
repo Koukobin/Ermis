2520b853011def26ab77a5a2aaf004fd64d7c7b8
Koukobin <ilias.koukovinis@gmail.com>
2026-05-09 23:59:19 -1100

    fix(server): enhance set_systemd_credential.sh robustness and portability

--- ErmisServer/Installers/Unix/DEB/ermis-server-installer_all/opt/ermis-server/set_systemd_credential.sh
+++ ErmisServer/Installers/Unix/DEB/ermis-server-installer_all/opt/ermis-server/set_systemd_credential.sh
@@ -1,28 +1,41 @@
 #!/bin/bash
 
-declare -a my_array=("server_key-store_password" "db_user_password" "db_key-store_password" "emailer_password")
+declare -a my_array=("server_key-store_password" "db_user_password" "db_key-store_password" "emailer_password" "exit")
 arraylength=${#my_array[@]}
+last_index=$(( arraylength - 1 ))
 
-# use for loop to read all values and indexes
-for (( i = 0; i < ${arraylength}; i++ ));
-do
-  echo "$i. ${my_array[$i]}"
+while :; do
+    # Use for loop to read all values and indices
+    for (( i = 0; i < ${arraylength}; i++ ));
+    do
+      echo "$i. ${my_array[$i]}"
+    done
+
+    read -p "Choose credential to set: " credential_to_set_index
+
+    if ! [[ "$credential_to_set_index" =~ ^[0-9]+$ ]]; then
+        echo "Please enter a valid number"
+        continue
+    fi
+
+    if (( credential_to_set_index == last_index )); then
+        exit 0
+    fi
+
+    if (( credential_to_set_index > last_index )); then
+        echo "Index does not exist"
+        continue
+    fi
+
+    credential_to_set=${my_array[$credential_to_set_index]}
+
+    read -s -p "Enter password for '$credential_to_set' credential: " password
+    printf '\n'
+
+    sudo printf '%s' "$password" > "${credential_to_set}.txt"
+    sudo systemd-creds encrypt "${credential_to_set}.txt" "${credential_to_set}.cred"
+    sudo shred -u "${credential_to_set}.txt"
+    sudo install -m 600 "${credential_to_set}.cred" \
+     /etc/ermis-server/
+    echo "Saved."
 done
-
-read -p "Choose credential to set: " credential_to_set_index
-
-if [ $credential_to_set_index -ge $arraylength ]; then
-    echo -e "\nIndex does not exist"
-    exit 1
-fi
-
-credential_to_set=${my_array[$credential_to_set_index]}
-
-read -s -p "Enter password for $credential_to_set credential: " password
-echo -e "\n"
-
-sudo echo "${password}" > "${credential_to_set}.txt"
-sudo systemd-creds encrypt "${credential_to_set}.txt" "${credential_to_set}.cred"
-sudo shred -u "${credential_to_set}.txt"
-sudo install -m 600 "${credential_to_set}.cred" \
- /etc/ermis-server/
